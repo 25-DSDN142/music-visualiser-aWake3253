24b212bc2cc12ba521cd14506213783c79b70121
aWake3253 <adamwake28@gmail.com>
2025-08-05 11:29:08 +1200

    experimented with maps

--- music_day1.js
+++ music_day1.js
@@ -1,24 +1,26 @@
-let sunY = 200
 
 function draw_one_frame(words, vocal, drum, bass, other, counter) {
     background(20)
     drumSize = map(drum, 0, 100, 50, 400);  
     bassSize = map(bass, 0, 100, 50, 400);  
+    wordSize = map(vocal,0, 700, 60, 600)
 
-    sunY += (bass-50) *0.2;
+
     fill(255, 100, 50)
    ellipse(width/2, height/2, bassSize, drumSize);
 
     fill(100, 100, 50);
    ellipse(width/2, height/2, drumSize, bassSize);
+    fill(167, 120, 30);
+
+   ellipse(width/2, drumSize, 100, 100)
    
-   if (sunY > 1000) {
-    sunY = 0}
 
-textAlign(CENTER);
+
+   textAlign(CENTER);
    textSize(vocal);
-   text(words, width/2, height/4);
-
+   fill(wordSize, drumSize/2, bassSize/2);
+   text(words, wordSize+width/3, height/4);
 
 
 
