dbea3a84df0636315235b8aa0850f10f461e3616
aWake3253 <adamwake28@gmail.com>
2025-08-23 12:43:31 +1200

    Added bar graph functionailty at bottom, currently working on adding color functionality

--- music_day1.js
+++ music_day1.js
@@ -1,25 +1,26 @@
-colorMode(HSB);
+let barHeights = [];
 function draw_one_frame(words, vocal, drum, bass, other, counter) {
     background(20)
-    drumSize = map(drum, 0, 100, 50, 400);  
-    bassSize = map(bass, 0, 100, 50, 400);  
-    wordSize = map(vocal,0, 700, 60, 600)
+    drawSpectrum(drum, bass, vocal, other, width, height, counter);
+
+    let drumSize = map(drum, 0, 100, 50, 400);  
+    let bassSize = map(bass, 0, 100, 50, 400);  
+    let wordSize = map(vocal,0, 700, 60, 600)
 
     let amplitude = -drumSize/1.1;
     let frequency = 0.007*-bassSize;
     let hueValue = (counter*2) %360;
     let changingColor = color(hueValue, 100, 100);
+    push();
     colorMode(RGB);
     for (var i = 0; i<5; i++){   
       let yOffset = i*40;
       let phase = i*100;
        drawWave(amplitude, frequency, counter, yOffset, phase);
       }
-
+    pop();
     drawEllipse(width, height, bassSize, drumSize);
-
     drawWords(words, wordSize, changingColor, width, height)
-    
 }
    
 function drawWave(amplitude, frequency, counter, yOffset, phase){
@@ -59,18 +60,36 @@ function drawEllipse(width, height, bassSize, drumSize){
             fill(100, 100, 50);
             ellipse(x, y, drumSize/2, bassSize/2);
       }
-      
     }
     fill(167, 120, 30);
   }
 }
 
+function drawSpectrum (drum, bass, vocal, other, width, height, counter){
+  colorMode(RGB);
+  let numBars = 60;
+  let barWidth = width/ numBars;
 
+  if (barHeights.length != numBars){
+    barHeights = new Array(numBars).fill(0);
+  }
 
-
-  
+  for (let i=0; i<numBars; i++){
+    let t = i/numBars;
+    let value = (
+      drum * (1-t) *0.8 + 
+      bass * sin(t*PI) + 
+      vocal *t*0.5 + 
+      other * noise(t*5, counter*0.01));
     
-
-
-
-
+    let targetH = map(value, 0, 100, 0, height*0.7);
+    barHeights[i] = lerp(barHeights[i], targetH, 0.2)
+  }
+noStroke();
+for (let i = 0; i<numBars; i++){
+  let h = barHeights[i];
+  let hueShift = ((counter *2 + i*5) % 360 +360) %360;
+  fill(hueShift, 100, 100, 80);
+  rect(i*barWidth, height-h, barWidth-2, h)
+  }
+}
