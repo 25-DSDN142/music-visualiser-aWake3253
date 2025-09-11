ced79b7163f68f0fc091ab259386b22e041087fe
aWake3253 <adamwake28@gmail.com>
2025-09-11 18:26:37 +1200

    Revert "fixed ellipse color functionality"
    This reverts commit 84310bfb3cb2cad1359a9a966418ebd7abf4b2cb.

--- music_day1.js
+++ music_day1.js
@@ -3,7 +3,6 @@ let barColors = [];
 let ellipseColors = [];
 
 function randomColorPalette() { //allows user to edit/add colours to the color palette
-  colorMode(RGB);
   let colorPalette = [
     color(74, 219, 200, 180),
     color(92, 171, 125, 180),
@@ -12,14 +11,11 @@ function randomColorPalette() { //allows user to edit/add colours to the color p
   return colorPalette[int(random(colorPalette.length))];
 }
 
-function chorusColor(counter, alpha = 120) {
+function chorusColor(counter) {
   colorMode(HSB);
-  let base = color((counter % 360), 100, 100);
-  let r = red(base);
-  let g = green(base);
-  let b = blue(base);
+  let c = color((counter % 360), 100, 100);
   colorMode(RGB);
-  return color(r, g, b, alpha);
+  return c;
 }
 
 function initialiseBarColors(numBars) {
@@ -70,9 +66,9 @@ function draw_one_frame(words, vocal, drum, bass, other, counter) {
   }
   pop();
 
-    if ((counter >= 2755 && counter <= 4500) || (counter >= 7100 && counter <= 8000)) { //chorus counter
-    drawEllipse(width, height, bassSize, drumSize, counter);
-    }
+  if ((counter >= 2755 && counter <= 4500) || (counter >= 7100 && counter <= 8000)) { //chorus counter
+    drawEllipse(width, height, bassSize, drumSize);
+  }
 
   if ((counter > 1 && counter <= 2700) || (counter >= 4500 && counter <= 7100) || (counter >= 8000 && counter <= 9841)) { //verse counter
     drawWords(words, wordSize, changingColor, width, height);
@@ -113,12 +109,9 @@ function drawEllipse(width, height, bassSize, drumSize, counter) {
       let y = ySpace * row;
       
         if((row+col)%2 ==0){
-          const idx = (row-1)*cols + (col-1)
-          const c1 = chorusColor(counter +idx*8, 120)
-          const c2 = chorusColor(counter +idx*8+180, 120)
-          fill(c1);
+          fill(ellipseColors[row - 1][col - 1][0]);
           ellipse(x, y, bassSize / 2, drumSize / 2);
-          fill(c2);
+          fill(ellipseColors[row - 1][col - 1][1]);
           ellipse(x, y, drumSize / 2, bassSize / 2);
 
         }
